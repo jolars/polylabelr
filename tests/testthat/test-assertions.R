0a2dbfd1b84f36149af53325aed4013b6f2941d3
Johan Larsson <johanlarsson@outlook.com>
2018-10-24 22:39:58 +0200

    Correct tests

--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -3,6 +3,6 @@ context("assertions")
 test_that("erroneous input throws", {
   x <- rbind(c(0, 1), c(1, 1), c(1, 0), c(0, 0))
 
-  expect_error(polylabel(centroid(x, precision = c(1, 0.5))))
-  expect_error(polylabel(centroid(x, precision = "f")))
+  expect_error(poi(x, precision = c(1, 0.5)))
+  expect_error(poi(x, precision = "f"))
 })
