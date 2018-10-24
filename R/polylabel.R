0db4c31ef038eaa255ff3b6807cddeaad704a355
Johan Larsson <johanlarsson@outlook.com>
2018-10-24 22:38:45 +0200

    Update description, example

--- R/polylabel.R
+++ R/polylabel.R
@@ -1,4 +1,12 @@
-#' Title
+#' Pole of Inaccessibility (Visual Center) of a Polygon
+#'
+#' This function computes and returns the approximate
+#' pole of inaccessibility for a polygon using a quadtree-based algorithm
+#' developed by the people from Mapbox.
+#'
+#' If there are any `NA` values in the input, they
+#' will be treated as separators for multiple paths (rings) of the
+#' polygon, mimicking the behavior of [graphics::polypath()].
 #'
 #' @param x a vector of x coordinates or a matrix or data.frame
 #'   of x and y coordinates, a list of components x and y, or
@@ -8,29 +16,41 @@
 #' @param precision the precision to use when computing the center
 #' @param ... arguments passed down to [grDevices::xy.coords()]
 #'
-#' @return A list with items `x` and `y`, representing the
-#'   centroid of the polygon(s).
+#' @return An augmented version of the output of [grDevices::xy.coords()],
+#'   namely a list with items
+#'   \item{`x`}{x coordinate of the center}
+#'   \item{`y`}{y coordinate of the center}
+#'   \item{`xlab`}{label for the x coordinate}
+#'   \item{`ylab`}{label for the y coordinate}
+#'   \item{`dist`}{distance to the enclosing polygon}
 #'
-#' @seealso [grDevices::xy.coords()], [graphics::polygon()]
+#' @source Garcia-Castellanos & Lombardo, 2007. Poles of inaccessibility: A
+#'   calculation algorithm for the remotest places on earth Scottish
+#'   Geographical Journal, Volume 123, 3, 227-233.
+#'   <https://dx.doi.org/10.1080/14702540801897809>
+#' @source <https://github.com/mapbox/polylabel>
+#' @source <https://blog.mapbox.com/a-new-algorithm-for-finding-a-visual-center-of-a-polygon-7c77e6492fbc>
+#'
+#' @seealso [grDevices::xy.coords()], [graphics::polypath()]
 #'
 #' @export
 #'
 #' @examples
-#' x <- rbind(c(2, 2),
-#'            c(2.5, 2),
-#'            c(2.5, 2.5),
-#'            c(2, 2.5),
-#'            c(NA, NA),
-#'            c(0, 0),
-#'            c(0, 1),
-#'            c(1, 1),
-#'            c(1, 0))
-#' centroid <- centroid(x, precision = 0.5)
-#' plot(x, type = 'n')
-#' polygon(x)
-#' points(centroid)
+#  # helper for plotting paths/polygons
+#' plot_path <- function(x, y, ...) {
+#'   plot.new()
+#'   plot.window(range(x, na.rm = TRUE), range(y, na.rm = TRUE))
+#'   polypath(x, y, ...)
+#' }
 #'
-centroid <- function(x, y = NULL, precision = 1.0, ...) {
+#' x <- c(5, 10, 10, 5, 5, 6, 6, 7, 7, 6, 8, 8, 9, 9, 8)
+#' y <- c(5, 5, 10, 10, 5, 6, 7, 7, 6, 6, 8, 9, 9, 8, 8)
+#'
+#' plot_path(x, y, col = "grey", border = NA)
+#'
+#' points(poi(x, y))
+#'
+poi <- function(x, y = NULL, precision = 1.0, ...) {
   xy <- grDevices::xy.coords(x, y, ...)
 
   stopifnot(is.numeric(precision),
@@ -39,26 +59,21 @@ centroid <- function(x, y = NULL, precision = 1.0, ...) {
   ends <- c(which(is.na(xy$x) | is.na(xy$y)), length(xy$x) + 1)
   n <- length(ends)
 
-  points <- vector("list", n)
+  polys <- vector("list", n)
   i <- start <- 1
 
   for (end in ends) {
     if (end > start) {
       ind <- start:(end - 1)
-      points[[i]] <- cbind(xy$x[ind], xy$y[ind])
+      polys[[i]] <- cbind(xy$x[ind], xy$y[ind])
       i <- i + 1
     }
     start <- end + 1
   }
 
-  centroids <- vapply(points, polyCentroid, double(2), precision = precision)
+  center <- poi_cpp(polys, precision)
 
-  # squared distances to edges of polygon
-  dist <- double(n)
-  for (i in 1:n) {
-    dist[i] <- min(colSums((t(points[[i]]) - centroids[, i])^2))
-  }
-
-  # return best distance
-  grDevices::xy.coords(t(centroids[, which.max(dist)]), ...)
+  out <- grDevices::xy.coords(center[1], center[2], ...)
+  out$dist <- center[3]
+  out
 }
